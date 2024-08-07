c4b79c3a11ba4c32c84c81456bb9b18632e2af93
esgoet <toddesgo@gmail.com>
2024-08-07 11:59:08 +0200

    add subject for grades

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,43 @@ type Student = {
     firstName: string,
     lastName: string,
     age: number,
-    grades: (GermanGrade | AmericanGrade | undefined)[];
+    grades: { subject : string, grades: (GermanGrade | AmericanGrade | undefined)[]}[];
 }
 
 type GermanGrade = 1 | 2 | 3 | 4 | 5 | 6;
 type AmericanGrade = "A" | "B" | "C" | "D" | "E" | "F";
 
+
 const student1: Student = {
     firstName: "Eva",
     lastName: "Rippe",
     age: 15,
-    grades: [1,"B",1,undefined, "A", "B"]
+    grades: [
+        {subject: "Chemistry", grades: [1,"B",1]},
+        {subject: "Math", grades: [undefined, "A", "B"]},
+        {subject: "Art", grades: [1,3,5]}
+    ]
 };
 const student2: Student = {
     firstName: "Adam",
     lastName: "Apfel",
     age: 17,
-    grades: [5,undefined,3,undefined, "C", "B"]
+    grades: [
+        {subject: "Chemistry", grades: [5,"E",3]},
+        {subject: "Math", grades: ["D", "B", 3, 4]},
+        {subject: "Art", grades: [undefined,3,undefined]}
+    ]
 }
 
 const student3: Student = {
     firstName: "Lilith",
     lastName: "Eden",
     age: 17,
-    grades: [2,1,undefined,"C", "A", 2]
+    grades: [
+        {subject: "Chemistry", grades: [1,"A",undefined]},
+        {subject: "Math", grades: [undefined, "A", "B"]},
+        {subject: "Art", grades: [1,2,2]}
+    ]
 }
 
 const outputStudent = (student: Student) : string => {
@@ -33,7 +46,7 @@ const outputStudent = (student: Student) : string => {
     for (let i:number = 0; i < 30; i++) {
         output += "="
     }
-    output += `\nGrades: ${student.grades.map((grade) => grade === undefined ? "*" : grade ).join(",")}`
+    output += `\nGrades:\n${student.grades.map((grade) => grade.subject + ": " + grade.grades.map((grade) => grade === undefined ? "*" : grade).join(",")).join("\n")}`;
     return output;
 }
 
