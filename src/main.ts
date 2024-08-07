b27728b603f5cf2fa4865120330781db0e51c443
esgoet <toddesgo@gmail.com>
2024-08-07 11:33:12 +0200

    add function for outputting student list

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,35 @@ type AmericanGrade = "A" | "B" | "C" | "D" | "E" | "F";
 
 const student1: Student = {
     firstName: "Eva",
-    lastName: "Goetzke",
-    age: 27,
+    lastName: "Rippe",
+    age: 15,
     grades: [1,"B",1,undefined, "A", "B"]
 };
-console.log(student1);
+const student2: Student = {
+    firstName: "Adam",
+    lastName: "Apfel",
+    age: 17,
+    grades: [5,undefined,3,undefined, "C", "B"]
+}
 
-const outputStudent = (student: Student) => {
+const student3: Student = {
+    firstName: "Lilith",
+    lastName: "Eden",
+    age: 17,
+    grades: [2,1,undefined,"C", "A", 2]
+}
+
+const outputStudent = (student: Student) : string => {
     let output: string = `${student.firstName} ${student.lastName} (${student.age})\n`;
     for (let i:number = 0; i < 30; i++) {
         output += "="
     }
     output += `\nGrades: ${student.grades.map((grade) => grade === undefined ? "*" : grade ).join(",")}`
-    console.log(output);
+    return output;
 }
 
-outputStudent(student1);
+const outputStudentList = (students: Student[]) : string => {
+    return students.map(student => outputStudent(student)).join("\n\n");
+}
+
+console.log(outputStudentList([student1, student2, student3]));
